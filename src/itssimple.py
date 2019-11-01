22bd1f1ab4c4df0ec9a3c0a118b2417c273dbbd0
Kevin Lin <kevin.lin@gameofapps.com>
2019-10-31 21:18:00 -0700

    Added config setup for anyone to use

--- src/itssimple.py
+++ src/itssimple.py
@@ -1,16 +1,22 @@
-import os
 import random
 from random import randint
 import tweepy
+import tweepy as tweepy
 from pytrends.request import TrendReq
+import json
 
 # Constants:
 NumOptions = 3
 
+# Open config file
+with open('config.json') as config_file:
+    config = json.load(config_file)
+
 # Tweepy authentication
-auth = tweepy.OAuthHandler(1,2)
-auth.set_access_token(1,2)
+auth = tweepy.OAuthHandler(config['keys']['api_key'], config['keys']['api_key_secret'])
+auth.set_access_token(config['keys']['consumer_key'], config['keys']['consumer_key_secret'])
 api = tweepy.API(auth)
+
 try:
     api.verify_credentials()
     print("Authentication OK")
@@ -19,25 +25,32 @@ except:
 
 
 # Helper methods
+# checks if the word has been used before, returns true if so
 def checkHistory(string):
     with open('search.txt') as file:
         if string in file.read():
-            print(string+ ' has been used already')
+            print(string + ' has been used already')
             return True
         else:
-            print(string+' hasn\'t been used before')
+            print(string + ' hasn\'t been used before')
             return False
 
+
+# writes the unused word to search.txt aka the history
 def addHistory(string):
     with open('search.txt', "a", encoding="utf-8") as file:
         file.write(string)
         file.write("\n")
 
+
+#set up shit
 printString = ""
 randomPick = randint(0, NumOptions)
 found = 1
 
+#find words until we find a new word
 while found < 2:
+    #The Google Trends outcome:
     pytrends = TrendReq(hl='en-US', tz=360)
     if randomPick == 0:
         year = randint(2006, 2018)
@@ -46,27 +59,29 @@ while found < 2:
         randomIndex = randint(0, 9)
         for index, row in top_charts_df.iterrows():
             if index == randomIndex:
-                if(checkHistory(row.title)):
+                if (checkHistory(row.title)):
                     print("Searching for new word")
                 else:
                     addHistory(row.title)
                     printString = row.title
                     found = 5
+    #The self-written quips outcome:
     elif randomPick == 1:
         lines = open("samples.txt").read().splitlines()
         print("Chose Samples")
         chosenSamplesString = random.choice(lines)
-        if(checkHistory(chosenSamplesString)):
+        if (checkHistory(chosenSamplesString)):
             print("Searching for new word")
         else:
             addHistory(chosenSamplesString)
             printString = chosenSamplesString
             found = 5
+    #The Card Against Humanity outcome
     else:
         lines = open("cah.txt", encoding="utf8").read().splitlines()
         print("Chose CAH")
         chosenCAHString = random.choice(lines)
-        if(checkHistory(chosenCAHString)):
+        if (checkHistory(chosenCAHString)):
             print("Searching for new word")
             break
         else:
@@ -74,15 +89,14 @@ while found < 2:
             printString = chosenCAHString
             found = 5
 
-
 api.update_status('It\'s simple, I play Barnes and I summon ' + str(printString))
 
 # related_queries_dict = pytrends.related_queries() related to games
 # print(related_queries_dict)
 
 # api.update_status("It's simple, I play Barnes and I summon")
-#kw_list = ["Games"]
-#pytrends.build_payload(kw_list, cat=41, timeframe='now 1-d', geo='', gprop='')
+# kw_list = ["Games"]
+# pytrends.build_payload(kw_list, cat=41, timeframe='now 1-d', geo='', gprop='')
 
 # trending_searches_df = pytrends.trending_searches() #trending searches right now
 # print(trending_searches_df.head())
