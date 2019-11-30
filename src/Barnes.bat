25f141c006dfa2bb9d08d39278045ede59527257
Kevin Lin <kevin.lin@gameofapps.com>
2019-11-30 11:57:13 -0800

    Added documentation to bat file

--- src/Barnes.bat
+++ src/Barnes.bat
@@ -1,2 +1,5 @@
+::This file allows you to run the script from your desktop using cmd instead of an IDE like Pycharm
+::Replace these with your python.exe and your itssimple script file.
+::Then, make a shortcut on desktop and change icon to https://drive.google.com/file/d/1tfVKjh8WH_nYSSR0f1a8hTGcjJo0CKhF/view?usp=sharing
 C:\Users\Kevin\AppData\Local\Programs\Python\Python37\python.exe "C:\Users\Kevin\PycharmProjects\PlayBarnesBot\src\itssimple.py"
 pause
